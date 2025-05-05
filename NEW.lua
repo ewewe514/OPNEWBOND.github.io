e10ea6d86ae668b888ce4b43c830bfda4935d20a
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-05 19:37:31 -0400

    Update NEW.lua

--- NEW.lua
+++ NEW.lua
@@ -1,1 +1,11 @@
-e
+task.spawn(function()
+    task.wait(7) -- Wait 5 seconds before executing
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ewewe514/bywe.github.io/refs/heads/main/ak.lua"))()
+end)
+
+
+task.spawn(function()
+    task.wait(12) -- Wait 5 seconds before executing
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ewewe514/NEWOP.github.io/refs/heads/main/erj.lua"))()
+end)
+
